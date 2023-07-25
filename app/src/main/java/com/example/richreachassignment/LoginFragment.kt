ff51706f62f48194a014222419238259985b3cde
akashp0ddar <akashpoddar40372gmail.com>
2023-07-25 23:40:03 +0530

    Google auth is done now need to work api call and storage

--- app/src/main/java/com/example/richreachassignment/LoginFragment.kt
+++ app/src/main/java/com/example/richreachassignment/LoginFragment.kt
@@ -1,14 +1,31 @@
 package com.example.richreachassignment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.richreachassignment.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignClient: GoogleSignInClient
+    private lateinit var signInLauncher: ActivityResultLauncher<Intent>
+
     private val viewModel by activityViewModels<MainViewModel> {
         ViewModelFactory(
             viewModelProviderRepository = Repository()
@@ -18,10 +35,78 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
+        registerLaunchers()
+        fireBaseSetup()
         onClick()
+    }
+
+    private fun registerLaunchers() {
+        signInLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val details = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleResult(details)
+                } else {
+                    Log.d(
+                        "googleSignResult",
+                        "registerLaunchers:${result.data} ${result.resultCode} ${
+                            GoogleSignIn.getSignedInAccountFromIntent(
+                                result.data
+                            )
+                        }"
+                    )
+                }
+
+            }
+    }
+
+    private fun handleResult(signedInAccountFromIntent: Task<GoogleSignInAccount>) {
+        if (signedInAccountFromIntent.isSuccessful) {
+            Toast.makeText(
+                requireContext(),
+                "${signedInAccountFromIntent.result.displayName}",
+                Toast.LENGTH_SHORT
+            ).show()
+            updateUi(signedInAccountFromIntent.result)
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "${signedInAccountFromIntent.exception}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+    }
+
+    private fun updateUi(result: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(result.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener { taskAuthResult ->
+            if (taskAuthResult.isSuccessful) {
+                Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Not saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun fireBaseSetup() {
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+
+        googleSignClient = GoogleSignIn.getClient(requireActivity(), gso)
     }
 
     private fun onClick() {
 
+        binding.btnLogin.setOnClickListener {
+            signInGoogle()
+        }
+
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignClient.signInIntent
+        signInLauncher.launch(signInIntent)
     }
 }
